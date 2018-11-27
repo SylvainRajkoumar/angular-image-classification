6cbbe98bd375fe95f4842f9da0bed9836aae83b5
Rajkoumar <sylvain.rajkoumar@gmail.com>
2018-11-27 16:59:02 +0100

    .

--- src/app/composants/mnist/mnist.component.ts
+++ src/app/composants/mnist/mnist.component.ts
@@ -11,38 +11,48 @@ export class MnistComponent {
   constructor(public monitoringService: MonitoringService) {
     this.monitoringService.incrementMnistUsage();
    }
-
+  
   selectedFile :File = null;
   
   public chartType:string = 'bar';
 
-    public chartDatasets:Array<any> = [
-        {data: [0, 59, 80, 81, 56, 55, 40, 50, 50, 50], label: 'Tensorflow Classification Percentage'}
-    ];
+  public chartDatasets:Array<any> = [
+      {data: [0, 59, 80, 81, 56, 55, 40, 50, 50, 50], label: 'Tensorflow Classification Percentage'}
+  ];
 
-    public chartLabels:Array<any> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  public chartLabels:Array<any> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    public chartColors:Array<any> = [
-        {
-            backgroundColor: 'rgba(0,0,0,0.2)',
-            borderColor: 'rgba(255,0,0,1)',
-            borderWidth: 2,
-            pointBackgroundColor: 'rgba(220,220,220,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(220,220,220,1)'
-        }
-    ];
+  public chartColors:Array<any> = [
+      {
+          backgroundColor: 'rgba(0,0,0,0.2)',
+          borderColor: 'rgba(255,0,0,1)',
+          borderWidth: 2,
+          pointBackgroundColor: 'rgba(220,220,220,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(220,220,220,1)'
+      }
+  ];
 
-    public chartOptions:any = {
-        responsive: true
-    };
-    public chartClicked(e: any): void { }
-    public chartHovered(e: any): void { }
+  public chartOptions:any = {
+      responsive: true
+  };
+
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
 
   onFileSelected(event){
     this.selectedFile = <File>event.target.files[0];
     console.log(event);
+    this.chartDatasets[0].data = [0, 59, 1000, 81, 56, 55, 40, 50, 50, 50];
+    console.log(this.chartDatasets[0].data);
+    this.chartClicked(5):
+    // path = null;
+    // this.tensorflowProcessing(path);
+  }
+
+  tensorflowProcessing(imagePath){
+
   }
 
 
