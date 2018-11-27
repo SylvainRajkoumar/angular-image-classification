5b94fcfdb3ac538bf8b8fd85443e281647266432
Rajkoumar <sylvain.rajkoumar@gmail.com>
2018-11-27 15:00:59 +0100

    pie chart removed

--- src/app/composants/stats/stats.component.ts
+++ src/app/composants/stats/stats.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../services/session.service';
 import { Router } from '@angular/router';
 import { MonitoringService } from '../../services/monitoring.service';
-import { Chart } from 'mdbootstrap';
 
 @Component({
   selector: 'app-stats',
@@ -17,24 +16,11 @@ export class StatsComponent implements OnInit {
       this.router.navigate(['/login']);
       console.log("DECONNECTE")
     } 
+    this.updatePieChart()
   }
 // Créer une fonction pour afficher le nombre d'utilisation via le monitoring
   updatePieChart(){
-    let ctxP = document.getElementById("pieChart").getContext('2d');
-    let myPieChart = new Chart(ctxP, {
-      type: 'pie',
-      data: {
-        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-        datasets: [{
-          data: [300, 50, 100, 40, 120],
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-        }]
-      },
-      options: {
-        responsive: true
-      }
-    });
+
   }
   ngOnInit() {
   }
